5c0f8dba96e8564cdfc895d1e41c63c999317844
Pradipto Chowdhury <90888280+Pradiptocodes1@users.noreply.github.com>
2022-11-04 22:22:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+let val = document.querySelectorAll(".drum").length;
+for(var i=0;i<val;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var Letter = this.innerHTML;
+        makesound(Letter);
+        buttonAnimation(Letter);
+    });
+    }
+
+document.addEventListener("keydown",function(event) {
+    makesound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makesound(key){
+    switch(key){
+        case "w":
+        var crash = new Audio("./sounds/crash.mp3")
+        crash.play();
+        break;
+        case "a":
+        var kick = new Audio("sounds/kick-bass.mp3")
+        kick.play();
+        break;
+        case "s":
+        var snare = new Audio("sounds/snare.mp3")
+        snare.play();
+        break;
+        case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3")
+        tom1.play();
+        break;
+        case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3")
+        tom2.play();
+        break;
+        case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3")
+        tom3.play();
+        break;
+        case "L":
+        var tom4 = new Audio("sounds/tom-4.mp3")
+        tom4.play();
+        break;
+        default:
+        console.log();
+    }
+}
+function buttonAnimation(key){
+    var activekey=document.querySelector("."+key);
+    activekey.classList.add("active");
+    setTimeout(function(){
+        activekey.classList.remove("active");
+    },100)
+}
+
